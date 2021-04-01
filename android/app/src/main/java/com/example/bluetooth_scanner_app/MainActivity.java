4b5b66cbd28a2b0a15609a9874d217f416530835
Etornam Sunu <sunumacbright@gmail.com>
2021-04-01 22:20:26 +0000

    first commit

---
+++ android/app/src/main/java/com/example/bluetooth_scanner_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.bluetooth_scanner_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatefulWidget {
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+final FlutterBlue _flutterBlue = FlutterBlue.instance;
+
+
+@override
+  void initState() {
+    
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Bluetooth scanner',style: TextStyle(color: Colors.white,fontSize: 26, fontWeight: FontWeight.bold),)
+      ),
+      body:  Column(
+        children: [
+          Spacer(),
+          Text('Tap to scan for Bluetooth devices',style: TextStyle(fontWeight: FontWeight.w600,fontSize: 18,color: Colors.white54),),
+          SizedBox(height: 10,),
+          Center(
+            child: CircleAvatar(
+              radius: 100,
+              backgroundColor: Colors.white,
+              child: Icon(Icons.bluetooth),
+            ),
+          ),
+          Spacer()
+        ],
+      ),
+    );
+  }
+}
